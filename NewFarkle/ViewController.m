7a36d17f6252e3a4405d704ebe0fcc56de108d7d
Albert Saucedo <saucedoa7@gmail.com>
2015-02-14 11:10:05 -0600

    1. As a user, I want to roll all the dice
    2. As a user, I want to select dice to roll again

--- NewFarkle/ViewController.m
+++ NewFarkle/ViewController.m
@@ -21,32 +21,74 @@
 
 -(int)roll{
     int randoNumber = arc4random()%6+1;
-
-    NSLog(@"rando is %ld", (long)randoNumber);
     return randoNumber;
 }
 
 -(IBAction)onDie1Tapped:(id)sender{
-    self.lblDieLabel1.text = [NSString stringWithFormat:@"%d", [self roll]];
+    self.lblDieLabel1.userInteractionEnabled = NO;
+    self.lblDieLabel1.backgroundColor = [UIColor colorWithRed:0.22 green:0.3 blue:0.44 alpha:1];
 }
 
 -(IBAction)onDie2Tapped:(id)sender{
-    self.lblDieLabel2.text = [NSString stringWithFormat:@"%d", [self roll]];
+    self.lblDieLabel2.userInteractionEnabled = NO;
+    self.lblDieLabel2.backgroundColor = [UIColor colorWithRed:0.22 green:0.3 blue:0.44 alpha:1];
 }
 
 -(IBAction)onDie3Tapped:(id)sender{
-    self.lblDieLabel3.text = [NSString stringWithFormat:@"%d", [self roll]];
+    self.lblDieLabel3.userInteractionEnabled = NO;
+    self.lblDieLabel3.backgroundColor = [UIColor colorWithRed:0.22 green:0.3 blue:0.44 alpha:1];
 }
 
 -(IBAction)onDie4Tapped:(id)sender{
-    self.lblDieLabel4.text = [NSString stringWithFormat:@"%d", [self roll]];
+    self.lblDieLabel4.userInteractionEnabled = NO;
+    self.lblDieLabel4.backgroundColor = [UIColor colorWithRed:0.22 green:0.3 blue:0.44 alpha:1];
 }
 
 -(IBAction)onDie5Tapped:(id)sender{
-    self.lblDieLabel5.text = [NSString stringWithFormat:@"%d", [self roll]];
+    self.lblDieLabel5.userInteractionEnabled = NO;
+    self.lblDieLabel5.backgroundColor = [UIColor colorWithRed:0.22 green:0.3 blue:0.44 alpha:1];
 }
 
 -(IBAction)onDie6Tapped:(id)sender{
-    self.lblDieLabel6.text = [NSString stringWithFormat:@"%d", [self roll]];
+    self.lblDieLabel6.userInteractionEnabled = NO;
+    self.lblDieLabel6.backgroundColor = [UIColor colorWithRed:0.22 green:0.3 blue:0.44 alpha:1];
 }
+
+- (IBAction)onRollButtonPressed:(id)sender {
+
+    if (self.lblDieLabel1.userInteractionEnabled == NO) {
+
+    } else {
+        self.lblDieLabel1.text = [NSString stringWithFormat:@"%d", [self roll]];
+    }
+
+    if (self.lblDieLabel2.userInteractionEnabled == NO) {
+
+    } else {
+        self.lblDieLabel2.text = [NSString stringWithFormat:@"%d", [self roll]];
+    }
+    if (self.lblDieLabel3.userInteractionEnabled == NO) {
+
+    } else {
+        self.lblDieLabel3.text = [NSString stringWithFormat:@"%d", [self roll]];
+    }
+    if (self.lblDieLabel4.userInteractionEnabled == NO) {
+
+    } else {
+        self.lblDieLabel4.text = [NSString stringWithFormat:@"%d", [self roll]];
+    }
+    if (self.lblDieLabel5.userInteractionEnabled == NO) {
+
+    } else {
+        self.lblDieLabel5.text = [NSString stringWithFormat:@"%d", [self roll]];
+    }
+    if (self.lblDieLabel6.userInteractionEnabled == NO) {
+
+    } else {
+        self.lblDieLabel6.text = [NSString stringWithFormat:@"%d", [self roll]];
+    }
+
+
+}
+
 @end
