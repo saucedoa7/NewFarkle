9ab8eb82c3fdb74f1089521470067272cb60520d
Albert Saucedo <saucedoa7@gmail.com>
2015-02-13 19:28:57 -0600

    first commit

---
+++ DieLabel.h
@@ -0,0 +1,13 @@
+//
+//  DieLabel.h
+//  NewFarkle
+//
+//  Created by Albert Saucedo on 2/13/15.
+//  Copyright (c) 2015 Albert Saucedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DieLabel : UILabel
+
+@end
---
+++ DieLabel.m
@@ -0,0 +1,21 @@
+//
+//  DieLabel.m
+//  NewFarkle
+//
+//  Created by Albert Saucedo on 2/13/15.
+//  Copyright (c) 2015 Albert Saucedo. All rights reserved.
+//
+
+#import "DieLabel.h"
+
+@implementation DieLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
